58f669b5e8a165f925ea6049611233cf46288f7c
webdevratul <mahibulislamratul@gmail.com>
2023-12-04 13:19:25 +0600

    get data from the database and added vercel json file

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
 
         const brandCollection = client.db("ElectraPulse").collection("brand");
         const productDataCollection = client.db("ElectraPulse").collection("product");
+        const addToCartCollection = client.db("ElectraPulse").collection("addToCart");
 
 
         app.get("/brands", async (req, res) => {
@@ -49,6 +50,31 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await brandCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.get("/productData/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productDataCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.post("/addCart", async (req, res) => {
+            const product = req.body;
+            const result = await addToCartCollection.insertOne(product);
+            res.send(result);
+        });
+
+        app.get("/myCarts", async (req, res) => {
+            const result = await addToCartCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.delete("/myCarts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await addToCartCollection.deleteOne(query);
             res.send(result);
         });
 
